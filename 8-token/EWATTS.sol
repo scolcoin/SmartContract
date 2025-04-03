7474a90f60acf13402a733c8f2e6cd2ad468b0aa
Social Coin of Colombia (SCOL) <33010917+scolcoin@users.noreply.github.com>
2025-04-02 21:00:02 -0500

    Update EWATTS.sol

--- 8-token/EWATTS.sol
+++ 8-token/EWATTS.sol
@@ -5,7 +5,7 @@
 /// * @website www.ewatts.co
 /// * @custom:whatsapp +57 3205032082
 /// * @custom: OPT 5000
-/// * @custm: 0x0aC7B8794BB874ba96827508003CDF91AF66Ba13
+/// * @custm: 0xC618f2d90655Dbe11593298f1E225fDb69B4527C
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.18;
 
