93171c853dd958ccdb6b9da4c702be7c3d8776de
Social Coin of Colombia (SCOL) <33010917+scolcoin@users.noreply.github.com>
2025-04-02 21:00:08 -0500

    Update ewatts_flattened.sol

--- 8-token/flatten/ewatts_flattened.sol
+++ 8-token/flatten/ewatts_flattened.sol
@@ -5,7 +5,7 @@
 /// * @website www.ewatts.co
 /// * @custom:whatsapp +57 3205032082
 /// * @custom: OPT 5000
-/// * @custm: 0x0aC7B8794BB874ba96827508003CDF91AF66Ba13
+/// * @custm: 0xC618f2d90655Dbe11593298f1E225fDb69B4527C
 // SPDX-License-Identifier: MIT
 
 // File: https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.9.0/contracts/utils/Context.sol
